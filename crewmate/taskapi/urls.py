81d512ef61409d70addaf38ee36e6c5d99f5f8b7
Colin Sullivan <colinsully@msn.com>
2021-01-23 00:20:31 -0600

    Expanded api

--- crewmate/taskapi/urls.py
+++ crewmate/taskapi/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 from rest_framework import routers
-from .views import PlayerTasksViewSet, TasksViewSet, PlayerViewSet, CalendarViewSet
+from .views import PlayerTasksViewSet, TasksViewSet, PlayerViewSet, CalendarViewSet, PlayerListViewSet, OccurrenceViewSet
 
 router = routers.DefaultRouter()
 router.register('playertasks', PlayerTasksViewSet)
 router.register('tasks', TasksViewSet)
-router.register('players', PlayerViewSet)
+router.register('playerlist', PlayerListViewSet)
+router.register('player', PlayerViewSet)
 router.register('calendar', CalendarViewSet)
+router.register('occurrences', OccurrenceViewSet)
 
 urlpatterns = router.urls
--- crewmate/taskapi/views.py
+++ crewmate/taskapi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from taskcalendar.models import Task, Player
 from schedule.models import Calendar, Occurrence
+from schedule.periods import Period, Week, Day, Month
+import datetime
 
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework import viewsets
@@ -26,7 +28,7 @@ class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class PlayerViewSet(viewsets.ModelViewSet):
+class PlayerListViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
@@ -34,9 +36,40 @@ class OccurrenceViewSet(viewsets.ModelViewSet):
     queryset = Occurrence.objects.all()
     serializer_class = OccurrenceSerializer
 
+    def list(self, request):
+        playerid = self.request.GET.get('playerid')
+        p = Player.objects.get(id=playerid)
+        c = p.calendar
+        events = Task.objects.filter(calendar=c)
+
+        now = datetime.datetime.now(datetime.timezone.utc)
+        period = self.request.GET.get('period')
+
+        p = None
+        if period == 'day':
+            p = Day(events, now)
+        if period == 'week':
+            p = Week(events, now)
+        queryset = p.get_occurrences()
+        serializer = OccurrenceSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
 class CalendarViewSet(viewsets.ModelViewSet):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
+
+    def list(self, request):
+        slug = self.request.GET.get('slug')
+        queryset = Calendar.objects.all()
+        if slug != None:
+            queryset = queryset.filter(slug=slug)
+        serializer = CalendarSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
 
     def list(self, request):
         pk = self.request.GET.get('id')
